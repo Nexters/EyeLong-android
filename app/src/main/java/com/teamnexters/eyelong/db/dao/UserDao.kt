663cf0b49a9b9c5dfad7da42975037634f7fefb8
ggahui <dbrkgml97@nate.com>
2020-08-04 23:16:08 +0900

    Add UserDao

--- app/src/main/java/com/teamnexters/eyelong/db/dao/UserDao.kt
+++ app/src/main/java/com/teamnexters/eyelong/db/dao/UserDao.kt
@@ -1,9 +1,17 @@
 package com.teamnexters.eyelong.db.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.teamnexters.eyelong.db.entity.User
 
 @Dao
 interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertUser(user: User)
+
+    
 
 
 }

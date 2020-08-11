f7454427d363e2c1f5da8d88091896989be81530
dlwlrma <io.dlwlrma@gmail.com>
2020-08-11 13:51:17 +0900

    enabled databinding and MainViewModel

---
+++ app/src/main/java/com/teamnexters/eyelong/ui/main/activity/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.teamnexters.eyelong.ui.main.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.teamnexters.eyelong.BR
+import com.teamnexters.eyelong.R
+import com.teamnexters.eyelong.databinding.ActivityMainBinding
+import com.teamnexters.eyelong.db.AppDatabase
+import com.teamnexters.eyelong.ui.main.viewmodel.MainViewModel
+
+class MainActivity : AppCompatActivity() {
+    private val mainViewModel = MainViewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO Room database init..
+        AppDatabase.getAppDatabase(applicationContext)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+        binding.setVariable(BR.viewModel, mainViewModel)
+    }
+}
---
+++ app/src/main/java/com/teamnexters/eyelong/ui/main/viewmodel/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.teamnexters.eyelong.ui.main.viewmodel
+
+class MainViewModel {
+}

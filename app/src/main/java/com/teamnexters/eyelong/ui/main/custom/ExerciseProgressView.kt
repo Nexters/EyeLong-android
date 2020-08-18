0b6d0acfa96c072f4419472fe319cfed34168837
dlwlrma <io.dlwlrma@gmail.com>
2020-08-19 04:57:45 +0900

    databinding test..

--- app/src/main/java/com/teamnexters/eyelong/ui/main/custom/ExerciseProgressView.kt
+++ app/src/main/java/com/teamnexters/eyelong/ui/main/custom/ExerciseProgressView.kt
@@ -32,89 +32,76 @@ class ExerciseProgressView : LinearLayout {
     private fun initView(context: Context?) {
         context?.let {
             val inflater = it.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            inflater.inflate(R.layout.layout_main_exercise, this@ExerciseProgressView, false).apply {
-                addView(this)
-            }
+            inflater.inflate(R.layout.layout_main_exercise, this@ExerciseProgressView, false)
+                .apply {
+                    addView(this)
+                }
         }
     }
 
     private fun initView(context: Context?, attrs: AttributeSet?) {
-        context?.let { context ->
-            val inflater =
-                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            inflater.inflate(R.layout.layout_main_exercise, this@ExerciseProgressView, false).apply {
-                addView(this)
+        initView(context)
+    }
 
-                context.theme.obtainStyledAttributes(
-                    attrs,
-                    R.styleable.ExerciseProgressView,
-                    0, 0
-                ).run {
-                    val max = getInteger(R.styleable.ExerciseProgressView_max, 7)
-                    var progress = getInteger(R.styleable.ExerciseProgressView_progress, 0)
-                    progress = if (progress <= max) progress else max
+    fun setProgress(progress: Int) {
+        with(findViewById<ImageView>(R.id.img_main_exercise_character)) {
+            val xPos = (progress - 1) * 64f
 
-                    with(findViewById<ImageView>(R.id.img_main_exercise_character)) {
-                        val xPos = (progress - 1) * 64f
+            translationX = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                xPos,
+                resources.displayMetrics
+            )
+        }
 
-                        translationX = TypedValue.applyDimension(
+        with(findViewById<View>(R.id.today_main_exercise)) {
+            val xPos = progress * 64f
+
+            translationX = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                xPos,
+                resources.displayMetrics
+            )
+        }
+
+        with(findViewById<LinearLayout>(R.id.progress_main_exercise)) {
+            for (index in 1..progress) {
+                View(context).apply {
+                    layoutParams = LayoutParams(
+                        LayoutParams.MATCH_PARENT,
+                        LayoutParams.WRAP_CONTENT
+                    ).apply {
+                        width = TypedValue.applyDimension(
                             TypedValue.COMPLEX_UNIT_DIP,
-                            xPos,
+                            64f,
                             resources.displayMetrics
-                        )
+                        ).toInt()
                     }
 
-                    with(findViewById<View>(R.id.today_main_exercise)) {
-                        val xPos = progress * 64f
-
-                        translationX = TypedValue.applyDimension(
-                            TypedValue.COMPLEX_UNIT_DIP,
-                            xPos,
-                            resources.displayMetrics
-                        )
-                    }
-
-                    with(findViewById<LinearLayout>(R.id.progress_main_exercise)) {
-                        for (index in 1..progress) {
-                            View(context).apply {
-                                layoutParams = LayoutParams(
-                                    LayoutParams.MATCH_PARENT,
-                                    LayoutParams.WRAP_CONTENT
-                                ).apply {
-                                    width = TypedValue.applyDimension(
-                                        TypedValue.COMPLEX_UNIT_DIP,
-                                        64f,
-                                        resources.displayMetrics
-                                    ).toInt()
-                                }
-
-                                background = context.getDrawable(
-                                    when (index) {
-                                        1 -> R.drawable.bg_main_exercise_progress_start
-                                        max -> R.drawable.bg_main_exercise_progress_end
-                                        else -> R.drawable.bg_main_exercise_progress_do
-                                    }
-                                )
-                            }.also {
-                                addView(it)
-                            }
+                    background = context.getDrawable(
+                        when (index) {
+                            1 -> R.drawable.bg_main_exercise_progress_start
+                            7 -> R.drawable.bg_main_exercise_progress_end
+                            else -> R.drawable.bg_main_exercise_progress_do
                         }
-                    }
-
-                    with(findViewById<LinearLayout>(R.id.week_main_exercise)) {
-                        children.map { it as TextView }.forEach {
-                            it.setTextColor(
-                                context.getColor(
-                                    if (indexOfChild(it) == progress) {
-                                        R.color.colorWhite
-                                    } else {
-                                        R.color.colorBlack
-                                    }
-                                )
-                            )
-                        }
-                    }
+                    )
+                }.also {
+                    addView(it)
                 }
+            }
+        }
+
+        with(findViewById<LinearLayout>(R.id.week_main_exercise)) {
+            children.map { it as TextView }.forEach {
+                it.setTextColor(
+                    context.getColor(
+                        if (indexOfChild(it) == progress) {
+                            R.color.colorWhite
+                        } else {
+                            R.color.colorBlack
+                        }
+                    )
+                )
             }
         }
     }
--- app/src/main/java/com/teamnexters/eyelong/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/teamnexters/eyelong/ui/main/viewmodel/MainViewModel.kt
@@ -1,4 +1,11 @@
 package com.teamnexters.eyelong.ui.main.viewmodel
 
+import androidx.databinding.ObservableField
+
 class MainViewModel {
+    val exerciseHistoryCount = ObservableField<Int>()
+
+    init {
+        exerciseHistoryCount.set(5)
+    }
 }

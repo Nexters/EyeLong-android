0421773af7b0425459ac5a373fc2e3e37f808f09
dlwlrma <io.dlwlrma@gmail.com>
2020-08-19 17:07:03 +0900

    activity_habit_check.xml and optimize code

---
+++ app/src/main/java/com/teamnexters/eyelong/ui/habit/activity/HabitCheckActivity.kt
@@ -0,0 +1,12 @@
+package com.teamnexters.eyelong.ui.habit.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HabitCheckActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_habit_check)
+    }
+}

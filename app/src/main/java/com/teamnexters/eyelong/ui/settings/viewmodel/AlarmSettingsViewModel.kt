9bf013ca64b472e37c9a121709253b5475f71211
dlwlrma <io.dlwlrma@gmail.com>
2020-09-08 10:39:54 +0900

    add TimePickerDialog for repeat

--- app/src/main/java/com/teamnexters/eyelong/ui/settings/viewmodel/AlarmSettingsViewModel.kt
+++ app/src/main/java/com/teamnexters/eyelong/ui/settings/viewmodel/AlarmSettingsViewModel.kt
@@ -5,21 +5,15 @@ import com.teamnexters.eyelong.wrapper.usecase.ActivityUseCase
 
 class AlarmSettingsViewModel(private val activityUseCase: ActivityUseCase) {
     fun onTimePickerStartClick() {
-        activityUseCase.showTimePickerDialog {
-            Log.i(
-                AlarmSettingsViewModel::class.qualifiedName,
-                it
-            )
-        }
+        activityUseCase.showTimePickerDialog { Log.i("TimePicker", it) }
     }
 
     fun onTimePickerEndClick() {
-        activityUseCase.showTimePickerDialog {
-            Log.i(
-                AlarmSettingsViewModel::class.qualifiedName,
-                it
-            )
-        }
+        activityUseCase.showTimePickerDialog { Log.i("TimePicker", it) }
+    }
+
+    fun onTimePickerRepeatClick() {
+        activityUseCase.showTimePickerDialog(true) { Log.i("TimePicker", it) }
     }
 
     fun onBackButtonClick() {

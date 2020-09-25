909ba19793f1f2fe49f0ed8e236ad3ba1a21c732
dlwlrma <io.dlwlrma@gmail.com>
2020-09-25 19:50:39 +0900

    intergrate google firebase

---
+++ app/src/main/java/com/teamnexters/eyelong/fcm/MyFirebaseMessagingService.kt
@@ -0,0 +1,14 @@
+package com.teamnexters.eyelong.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+    }
+}

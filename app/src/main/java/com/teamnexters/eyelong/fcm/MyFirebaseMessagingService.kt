273f41331a5187ea7eaf6736ba9787706eee0df5
dlwlrma <io.dlwlrma@gmail.com>
2020-09-27 22:22:23 +0900

    firebase cloud messaging with notification test

--- app/src/main/java/com/teamnexters/eyelong/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/teamnexters/eyelong/fcm/MyFirebaseMessagingService.kt
@@ -1,14 +1,55 @@
 package com.teamnexters.eyelong.fcm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.teamnexters.eyelong.R
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+
+        Log.i("FirebaseCloudMessaging", token)
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
+
+        notification(remoteMessage)
+        Log.i("FirebaseCloudMessaging", remoteMessage.notification.toString())
+    }
+
+    private fun notification(remoteMessage: RemoteMessage) {
+        val channelId = packageName
+        val notificationId = 0
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "alarm"
+            val channelDescription = "alarm notification channel for eyelong"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = channelDescription
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        var builder = NotificationCompat.Builder(this@MyFirebaseMessagingService, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Hello")
+            .setContentText("Hello")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this@MyFirebaseMessagingService)) {
+            notify(notificationId, builder.build())
+        }
     }
 }
